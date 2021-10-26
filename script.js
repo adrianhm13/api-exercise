82acd5c764ed28b2afd4ad4ecdee834757ca9a4c
Adrian Hervas <adrian.hervasmartinez@gmail.com>
2021-10-26 20:35:22 +0200

    Gif search bar, fetching function

---
+++ script.js
@@ -0,0 +1,33 @@
+const btn = document.getElementById("new-gif");
+btn.addEventListener('click', () => {getGif('cats')});
+
+const img = document.querySelector("img");
+
+const searchInput = document.getElementById('search-gif');
+searchInput.addEventListener('click', function(){this.select()});
+
+const searchBtn = document.getElementById('search-submit');
+searchBtn.addEventListener('click', () => {getInput()});
+
+function getInput(){
+    const inputSearch = searchInput.value;
+    getGif(inputSearch);
+}
+
+function getGif(input){
+    fetch(
+        `https://api.giphy.com/v1/gifs/translate?api_key=4UaTtBYWSgSVXhSCb4hBlDLlgHQrfZ2M&s=${input}`,
+        { mode: "cors" }
+      )
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (response) {
+          img.src = response.data.images.original.url;
+        })
+        .catch(function () {
+            alert(`It was impossible to find a gif with the word "${input}"!`)
+        });
+}
+
+getGif("cats");
